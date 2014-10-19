f379ec01dc4abaa87b39376efb448e5ca0330482
lmaisour <lmaisour@gmail.com>
2014-10-19 14:40:50 -0700

    added styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  # validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
   # Returns the hash digest of the given string.
   def User.digest(string)
---
+++ db/migrate/20141019213813_add_column_to_users.rb
@@ -0,0 +1,9 @@
+class AddColumnToUsers < ActiveRecord::Migration
+  def self.up
+    add_attachment :users, :avatar
+  end
+
+  def self.down
+    remove_attachment :users, :avatar
+  end
+end
