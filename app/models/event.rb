db49d40308743bca5e2bb282bf0d12c178e95cf3
lmaisour <lmaisour@gmail.com>
2014-10-29 13:41:28 -0700

    fixed button issue on event show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,13 @@
 class Event < ActiveRecord::Base
+
+	validates :title,  presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :location, presence: true
+	validates_uniqueness_of :attendee_id
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+
 	has_many :attendees
 	has_many :users, through: :attendees
 
